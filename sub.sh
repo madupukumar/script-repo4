ca60aee9339a4a74c6f4b15234fe7d0197c52e3b
madupukumar <madupusathish465@gmail.com>
2024-08-23 14:14:20 +0530

    msk2

---
+++ sub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ask user for minuend
+echo "Enter minuend:55 "
+
+# Ask user for subtrahend
+echo "Enter subtrahend:25 "
+
+# Perform subtraction
+difference=$((55 - 25))
+
+# Display result
+echo "Difference: $difference"
